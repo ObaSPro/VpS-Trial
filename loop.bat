70402d82f16c4a25472e3d96cc942675b8919fd9
ObaSPro <95392603+ObaSPro@users.noreply.github.com>
2021-12-05 13:07:28 +0800

    Add files via upload

---
+++ loop.bat
@@ -0,0 +1,8 @@
+@echo off
+echo RDP CREATION SUCCESSFULL!
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+:check
+ping 127.0.0.1 > null
+cls
+echo RDP CREATION SUCCESSFULL!
+goto check
